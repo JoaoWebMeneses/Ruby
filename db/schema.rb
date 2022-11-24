33ba325deff12011312d4f5d222b1aebe87d8b21
JoaoWebMeneses <joaowebmeneses@gmail.com>
2022-11-24 19:33:20 -0300

    diario em desenvolvimento

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_17_211226) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_213547) do
+  create_table "nota", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "conteudo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_nota_on_user_id"
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.text "conteudo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -24,4 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_17_211226) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "nota", "users"
+  add_foreign_key "tweets", "users"
 end
